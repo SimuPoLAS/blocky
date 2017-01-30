14652081388faf10bec1add3c87f9e24a43b275b
UgiNyaa <ugi@waifu.club>
2017-01-30 18:00:03 +0100

    bitwriter implemented

---
+++ include/bitwriter.hpp
@@ -0,0 +1,23 @@
+#ifndef BITWRITER_HPP
+#define BITWRITER_HPP
+
+#include <fstream>
+
+class BitWriter {
+private:
+    FILE* file;
+    uint8_t* buffer;
+    uint8_t offset;
+
+public:
+    BitWriter(FILE* file)
+        : file(file) {}
+
+    ~BitWriter();
+
+    void write(uint64_t data, uint8_t count);
+    void write(uint8_t data, uint8_t count);
+    void flush();
+};
+
+#endif /* end of include guard: BITWRITER_HPP */
---
+++ src/bitwriter.cpp
@@ -0,0 +1,43 @@
+#include <math.h>
+
+#include <bitwriter.hpp>
+
+BitWriter::~BitWriter()
+{
+    flush();
+    delete buffer;
+}
+
+void BitWriter::write(uint64_t data, uint8_t count)
+{
+    do
+    {
+        auto bitsLeft = 8 - offset;
+        if (bitsLeft > count)
+        {
+            *buffer |= (uint8_t)((data & uint64_t(pow(2, count) - 1)) << offset);
+            offset += count;
+            return;
+        }
+        *buffer |= (uint8_t)((data & uint64_t(pow(2, bitsLeft) - 1)) << offset);
+        fwrite(buffer, 1, 1, file);
+        offset = 0;
+        *buffer = 0;
+        count -= (uint8_t)bitsLeft;
+        data = data >> bitsLeft;
+        } while (count > 0);
+}
+
+void BitWriter::write(uint8_t data, uint8_t count)
+{
+    write(data, count);
+}
+
+void BitWriter::flush()
+{
+    if (*buffer == 0 && offset == 0)
+        return;
+    fwrite(buffer, 1, 1, file);
+    *buffer = 0;
+    offset = 0;
+}
